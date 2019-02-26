f849e383e1c84ac7a8f5071b7be930024f2499e9
priyanka5698 <priyankagokavarapu5698@gmail.com>
2019-02-26 14:23:21 +0530

    Changed db

--- django/priya/project1/settings.py
+++ django/priya/project1/settings.py
@@ -86,9 +86,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME' :'expansion',
+        'NAME' :'pta _expansion',
         'USER':'root',
-        'PASSWORD':'toor',
+        'PASSWORD':'priyoop666',
         'HOST':'localhost',
         'PORT':'3036'
 
