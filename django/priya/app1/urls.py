57c61119d52f8b547ca585a52842542d3c134f60
priyanka5698 <priyankagokavarapu5698@gmail.com>
2019-02-27 20:22:47 +0530

    Resolved login

--- django/priya/app1/urls.py
+++ django/priya/app1/urls.py
@@ -3,18 +3,18 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-     url(r'^home/$', views.home, name='home'),
-     url(r'^$', views.index, name='index'),
-     url(r'^login/$', views.login1, name='login1'),
-     url(r'^course/$', views.courses, name='courses'),
-    url(r'^aboutus/$', views.about, name='about'),
-    url(r'^contact/$', views.contactus, name='contactus'),
-    url(r'^registration/$', views.regform, name='regform'),
-     url(r'^logout/$',auth_views.logout,{'next_page': 'login1'}, name='logout'),
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+         url(r'^home/$', views.home, name='home'),
+         url(r'^$', views.index, name='index'),
+         url(r'^accounts/login/$', views.login1, name='login1'),
+         url(r'^course/$', views.courses, name='courses'),
+         url(r'^aboutus/$', views.about, name='about'),
+         url(r'^contact/$', views.contactus, name='contactus'),
+         url(r'^registration/$', views.regform, name='regform'),
+         url(r'^logout/$',auth_views.logout,{'next_page': 'login1'}, name='logout'),
+         url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+         url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+         url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+            auth_views.password_reset_confirm, name='password_reset_confirm'),
+         url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
 ]
